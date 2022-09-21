8f87a9c0cb130a56fbdf74d8eb6ff662cc8337c2
Michael Lee <vlmlee2@gmail.com>
2022-09-21 07:44:57 -0500

    Testing charts

--- frontend/src/components/Charts/Chart.tsx
+++ frontend/src/components/Charts/Chart.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import LineChart from "./LineChart";
 
-export default class Chart extends React.Component<any, any> {
-    constructor(props: any) {
-        super(props);
-    }
+export default function Chart() {
 
-    render() {
-        return <div></div>;
-    }
+    // @ts-ignore
+    const createLineChart = () => LineChart([
+        {date: 1, close: 93.24},
+        {date: 2, close: 99.92},
+        {date: 3, close: 111.98},
+        {date: "2007-05-20", close: 111.98}
+    ],
+        // @ts-ignore
+        {
+        x: (d: any) => d.date,
+        y: (d: any) => d.close,
+        yLabel: "↑ Daily close ($)",
+        width: 1200,
+        height: 500,
+        color: "steelblue"
+    });
+
+    return <div>
+        {createLineChart()}
+    </div>;
 }
