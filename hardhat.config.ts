6ad98c6383236d09a6333bc8231ee4076a25e268
Michael Lee <vlmlee2@gmail.com>
2022-10-16 02:43:31 -0500

    Need to wrap these in quotes to make them strings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,13 +10,13 @@ import "@typechain/ethers-v5";
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (args, hre) => {
-    const accounts = await hre.ethers.getSigners();
-    for (const account of accounts) {
-        console.log(account.address);
-    }
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
 });
 
-require('dotenv').config();
+require("dotenv").config();
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -25,36 +25,36 @@ require('dotenv').config();
  * @type import('hardhat/config').HardhatUserConfig
  */
 const config: HardhatUserConfig = {
-    react: {
-        providerPriority: ["web3modal", "hardhat"],
+  react: {
+    providerPriority: ["web3modal", "hardhat"],
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      inject: false, // optional. If true, it will EXPOSE your mnemonic in your frontend code. Then it would be available as an "in-page browser wallet" / signer which can sign without confirmation.
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk", // test test test test test test test test test test test junk
+      },
     },
-    networks: {
-        hardhat: {
-            chainId: 1337,
-            inject: false, // optional. If true, it will EXPOSE your mnemonic in your frontend code. Then it would be available as an "in-page browser wallet" / signer which can sign without confirmation.
-            accounts: {
-                mnemonic: "test test test test test test test test test test test junk", // test test test test test test test test test test test junk
-            },
+    sepolia: {
+      url: `${process.env.INFURA_URL + process.env.INFURA_API_KEY}`,
+      accounts: {
+        mnemonic: `${process.env.MNEMONIC}`,
+      },
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 50,
+          },
         },
-        sepolia: {
-            url: process.env.INFURA_URL + process.env.INFURA_API_KEY,
-            accounts: {
-                mnemonic: process.env.MNEMONIC
-            }
-        }
-    },
-    solidity: {
-        compilers: [
-            {
-                version: "0.8.9",
-                settings: {
-                    optimizer: {
-                        enabled: true,
-                        runs: 50,
-                    },
-                },
-            },
-        ],
-    },
+      },
+    ],
+  },
 };
 export default config;
