a4f78fbe8ca37ce912867e7f954125d7f3d58115
Michael Lee <vlmlee2@gmail.com>
2022-10-19 09:27:58 -0500

    Added starter about page. Added will-change style for transforms

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import '../stylesheets/AboutPage.scss';
 
 export default function About() {
-    return <div>A staking contract</div>;
+    return (
+        <div className={'about-page'}>
+            <div className={'about-page__content'}>
+                <div>This app is just one example of what you can do with a Leaderboard smart contract.</div>
+            </div>
+        </div>
+    );
 }
