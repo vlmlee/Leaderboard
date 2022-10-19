629f95f8f558ff49f59a3217aa981053c8e612e0
Michael Lee <vlmlee2@gmail.com>
2022-10-19 09:33:37 -0500

    Updated Rewards.tsx to use same styling as About.tsx

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -3,8 +3,8 @@ import '../stylesheets/AboutPage.scss';
 
 export default function About() {
     return (
-        <div className={'about-page'}>
-            <div className={'about-page__content'}>
+        <div className={'about-page info-sections'}>
+            <div className={'about-page__content info-sections__content'}>
                 <div>This app is just one example of what you can do with a Leaderboard smart contract.</div>
             </div>
         </div>
--- frontend/src/components/Rewards.tsx
+++ frontend/src/components/Rewards.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import '../stylesheets/Rewards.scss';
 
 const Rewards = ({}) => {
-    return <div>Rewards</div>;
+    return (
+        <div className={'rewards info-sections'}>
+            <div className={'rewards__content info-sections__content'}>rewards</div>
+        </div>
+    );
 };
 
 export default Rewards;
