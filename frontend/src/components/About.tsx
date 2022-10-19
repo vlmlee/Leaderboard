7f97d0e935346d35476a0c1eb512878f676634a8
Michael Lee <vlmlee2@gmail.com>
2022-10-19 10:04:57 -0500

    Updated about page

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -5,7 +5,9 @@ export default function About() {
     return (
         <div className={'about-page info-sections'}>
             <div className={'about-page__content info-sections__content'}>
-                <div>This app is just one example of what you can do with a Leaderboard smart contract.</div>
+                This app is just one example of what you can do with a Leaderboard smart contract. The design was
+                inspired (slightly) by <a href={'https://curve.fi/4pool'}>4pool</a>.
+                <br />
                 <br />
                 Come see the source code on <a href={'https://github.com/vlmlee/Leaderboard'}>github</a>.
             </div>
